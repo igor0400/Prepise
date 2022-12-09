f21d705ebd6de4468cfef81695f05a4bdf6cf8a4
Igor <prostopochtaivso@gmail.com>
2022-12-09 21:25:27 +0300

    made header template

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,21 +1,52 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import classNames from 'classnames';
 
 import { Layout } from 'antd';
 import Image from 'next/image';
 import Button, { Themes } from '../templates/Button';
 
 import logo from '../../public/logo100x56.svg';
-import plus from '../../public/icon/plus_.svg';
+import plus from '../../public/icons/plus_.svg';
+import search from '../../public/icons/search.svg';
 
 const { Header: AntHeader } = Layout;
 
 const Header: FC = () => {
+   const [inputFocus, setInputFocus] = useState(false);
+
    return (
       <AntHeader className="header flex justify-between">
          <Image src={logo} alt="logo" width={100} height={56} />
-         <div className="btns flex gap-3.5">
+         <div
+            className={classNames(
+               'search-input flex justify-between rounded-md ml-10',
+               { 'border-2': inputFocus, 'p-0.5': !inputFocus }
+            )}
+         >
+            <input
+               className="font-semibold text-base p-2.5 h-9 grow"
+               type="text"
+               placeholder="Поиск..."
+               onFocus={() => setInputFocus(true)}
+               onBlur={() => setInputFocus(false)}
+            />
+            <Image
+               className="mr-2.5 my-1.5 cursor-pointer"
+               src={search}
+               alt="search"
+               width={25}
+               height={25}
+            />
+         </div>
+         <div className="btns grow flex justify-end gap-3.5">
             <Button theme={Themes.SHADOW}>
-               <Image src={plus} alt="plus" width={20} height={20} className='mr-0.5' />
+               <Image
+                  src={plus}
+                  alt="plus"
+                  width={20}
+                  height={20}
+                  className="mr-0.5"
+               />
                Опубликовать
             </Button>
             <Button>Войти</Button>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,7 +4,7 @@ import PageWrapper from '../hoks/PageWrapper';
 const Home: NextPage = () => {
    return (
       <PageWrapper>
-         Главная
+         <h1>Удобная подготовка к собеседованиям</h1>
       </PageWrapper>
    );
 };
